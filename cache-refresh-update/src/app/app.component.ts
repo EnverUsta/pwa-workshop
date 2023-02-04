9719fc400dae50cae08d66a83ec354354296722b
EnverUsta <enveruusta@gmail.com>
2023-02-04 23:53:35 +0300

    feat(pwa&backend): add pwa and json-server

--- cache-refresh-update/src/app/app.component.ts
+++ cache-refresh-update/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'cache-refresh-update';
+export class AppComponent implements OnInit {
+  todos: any;
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.httpClient.get('http://localhost:3000/todos').subscribe((data) => {
+      console.log(data);
+      this.todos = data;
+    });
+  }
 }
